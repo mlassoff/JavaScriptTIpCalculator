e43c73bb568e4ad2f9082779effc030dee317224
Mark Lassoff <marklassoff@Marks-MBP.home>
2019-04-08 23:56:24 -0400

    Totalling Error

--- tip.js
+++ tip.js
@@ -1,6 +1,17 @@
 const range = document.getElementById("tipRange");
 const tipOut = document.getElementById("percentOut");
+const btnCalculate = document.getElementById("btnCalculate");
+const billAmount = document.getElementById("billAmount");
+const resultArea = document.getElementById("result");
 
 range.ondrag = function(){
-  tipOut.innerHTML = "" + range.value + "%";
+  tipOut.innerHTML =  range.value + "%";
+}
+
+btnCalculate.onclick = function(){
+  let theBill = parseFloat(billAmount.value);
+  let tip = (theBill * (range.value/100)).toFixed(2);
+  let total = theBill + tip;
+  let out = `<strong>Tip Amount:</strong> $${tip}<br/><strong>Total:</strong> $${total}`;
+  resultArea.innerHTML = out;
 }
